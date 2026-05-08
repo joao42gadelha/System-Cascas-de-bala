0d55584f17e4cb2950334ff64ca191fe91f33fc4
João Gadelha <joao42gadelha@gmail.com>
2026-05-07 20:22:29 -0400

    Add quantidadeEstoque field to Produto model

--- src/main/java/com/sistema/estoque/model/Produto.java
+++ src/main/java/com/sistema/estoque/model/Produto.java
@@ -21,15 +21,25 @@ public class Produto {
     @Column(unique = true)
     private String codigoBarras;
 
+    @Column(nullable = false)
+    private Integer quantidadeEstoque = 0; // Inicia zerado
+
     // Getters e Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+    
     public String getDescricao() { return descricao; }
     public void setDescricao(String descricao) { this.descricao = descricao; }
+    
     public Double getValor() { return valor; }
     public void setValor(Double valor) { this.valor = valor; }
+    
     public String getTipoProduto() { return tipoProduto; }
     public void setTipoProduto(String tipoProduto) { this.tipoProduto = tipoProduto; }
+    
     public String getCodigoBarras() { return codigoBarras; }
     public void setCodigoBarras(String codigoBarras) { this.codigoBarras = codigoBarras; }
+    
+    public Integer getQuantidadeEstoque() { return quantidadeEstoque; }
+    public void setQuantidadeEstoque(Integer quantidadeEstoque) { this.quantidadeEstoque = quantidadeEstoque; }
 }
